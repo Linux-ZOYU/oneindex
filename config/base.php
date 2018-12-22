483966e09369c27c6ae39e3a7c89c8d2698f8bcd
Linux @邹 彧 <40789517+Linux-ZOYU@users.noreply.github.com>
2018-12-23 01:15:13 +0800

    Add files via upload

---
+++ config/base.php
@@ -0,0 +1,106 @@
+<?php return array (
+  'site_name' => '',
+  'password' => '',
+  'style' => '',
+  'onedrive_root' => '/',
+  'cache_type' => 'secache',
+  'cache_expire_time' => 3600,
+  'cache_refresh_time' => 600,
+  'root_path' => '?',
+  'show' => 
+  array (
+    'stream' => 
+    array (
+      0 => 'txt',
+    ),
+    'image' => 
+    array (
+      0 => 'bmp',
+      1 => 'jpg',
+      2 => 'jpeg',
+      3 => 'png',
+      4 => 'gif',
+    ),
+    'video5' => 
+    array (
+      0 => 'mp4',
+      1 => 'webm',
+      2 => 'mkv',
+    ),
+    'video' => 
+    array (
+    ),
+    'video2' => 
+    array (
+      0 => 'avi',
+      1 => 'mpg',
+      2 => 'mpeg',
+      3 => 'rm',
+      4 => 'rmvb',
+      5 => 'mov',
+      6 => 'wmv',
+      7 => 'asf',
+      8 => 'ts',
+      9 => 'flv',
+    ),
+    'audio' => 
+    array (
+      0 => 'ogg',
+      1 => 'mp3',
+      2 => 'wav',
+    ),
+    'code' => 
+    array (
+      0 => 'html',
+      1 => 'htm',
+      2 => 'php',
+      3 => 'css',
+      4 => 'go',
+      5 => 'java',
+      6 => 'js',
+      7 => 'json',
+      8 => 'txt',
+      9 => 'sh',
+      10 => 'md',
+    ),
+    'doc' => 
+    array (
+      0 => 'csv',
+      1 => 'doc',
+      2 => 'docx',
+      3 => 'odp',
+      4 => 'ods',
+      5 => 'odt',
+      6 => 'pot',
+      7 => 'potm',
+      8 => 'potx',
+      9 => 'pps',
+      10 => 'ppsx',
+      11 => 'ppsxm',
+      12 => 'ppt',
+      13 => 'pptm',
+      14 => 'pptx',
+      15 => 'rtf',
+      16 => 'xls',
+      17 => 'xlsx',
+    ),
+  ),
+  'images' => 
+  array (
+    'home' => false,
+    'public' => false,
+    'exts' => 
+    array (
+      0 => 'jpg',
+      1 => 'png',
+      2 => 'gif',
+      3 => 'bmp',
+    ),
+  ),
+  'client_secret' => '',
+  'client_id' => '',
+  'redirect_uri' => 'https://ju.tn/',
+  'refresh_token' => 'OAQABAAAAAAC5una0EUFgTIF8ElaxtWjTbo6FqliDGttyLWGK2Z3m60owjr_kHvw41LoLLrLxi6UOrerFoBoSKC9fz3AANID4_K3ZpEz1UBjRwO6SkGdTGNQBpA_i2oRhmyTetTsAWXqyu3RXzh7C4Tlhipch1ThRSGfEMpaBrg4rOq39d6WpY2WrRwPfzBbgN-O8ykkhBhcyQfDmVk_UFZwJwuE2Ar1ERrVZ2GRu-oNGsBk_nHdAvGqoBLVrCMNZXha6DI69U_XpXfdhCe9rtc7sh2YH1oTq0piJXY15ezVTj0r45ys4AFCwO7YWH47OhAloDHFT1rs1rELKy-dPdUnirddBy-5a840yGI3bE8UNGu2cW35dYMQhHwITaIx2EU8xJ4TADQmWQ8I6lsD7OTLGJX4bCZYSN5dP1IQr13v4cZ35U622v__iqEwXWvVYhwFeXhkYbTbyICtn1ju9amcapf1nrukwaZoS6MuYv0iwYqBejStmJzJpI0m9FkleBne2jsiJgDHUFDbZxcf31LOhsmyZeDPLbt1q5zZWjNqoxmTWvDuZMc24MvNBlDmoFPBiBw0DK7djV4fdwlaHRIcShi6Mllml5Rb5mOlI3KJQshelhDF0n-ez0P705Sbhigpw6QlNk9LmNbLT3JHcGSSsr3oKMHW5IBALHET4PwSZcTjQgeHc1hCNLtfA2nLJeMv_NV2iOmVVrKLc8P9NakkFKYEVGbr0gL930wZATwfbhnFfWD3CufPg7ul5BC-LmxssNJldmcCzsd2V4kag6Eo2zexnnXmlIAA',
+  'onedrive_hide' => '',
+  'onedrive_hotlink' => '',
+);
